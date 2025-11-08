6e843090678ddc8cce402fa4299d4f08cfc8f893
mayahalabii <p9sgg4q22b@privaterelay.appleid.com>
2025-11-08 15:04:17 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,7 +1,7 @@
-def approximate_pi(n_terms):
+def approximate_pi(n_terms=10):
     pass # replace pass with your code
-    n_terms (10)
+
     pi = 0
-    for i range(n_terms) :
+    for i in range(n_terms) :
     pi += 4 * (((-1) ** i) / (2*i + 1))
     return pi
